229dceb2648e89861bada776183c90f1b693215b
Mustafa Bohra <92286277+mustafaBohra52@users.noreply.github.com>
2023-12-14 12:33:10 +0530

    settings.py

--- social_book/social_book/settings.py
+++ social_book/social_book/settings.py
@@ -82,7 +82,7 @@ DATABASES = {
         'NAME': 'todo',
         'USER': 'mustafabohra',
         'PASSWORD': 'mustafabohra',
-        'HOST': 'localhost',   # Set to the appropriate database host
+        'HOST': '35.77.48.137',   # Set to the appropriate database host
         'PORT': '5432',        # Set to the appropriate port for your PostgreSQL installation
     }
 }
